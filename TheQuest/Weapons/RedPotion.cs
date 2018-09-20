cdbfbbc15ba50c38b038be1239f90c7cf92110bb
rfajkowski <rfajkowski@gmail.com>
2018-09-20 21:18:06 +0100

    Initial commit

---
+++ TheQuest/Weapons/RedPotion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheQuest.Weapons
+{
+    class RedPotion : Weapon, IPotion
+    {
+        public bool Used { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
